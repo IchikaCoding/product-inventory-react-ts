439c4af57ae064b0e932a51b3126f72cb7435572
IchikaCoding <IchikaCoding@users.noreply.github.com>
2026-04-11 22:32:30 +0900

    add: 商品追加フォームコンポーネントのJSバージョンをコピペ

---
+++ src/components/AddProductForm.tsx
@@ -0,0 +1,166 @@
+import { useState } from "react";
+import {
+  validationPrice,
+  validationTrimmedName,
+} from "../validation/validation";
+import type { Product } from "../data/products";
+
+// ここでinputされた情報を受け取る必要がある！
+
+/**
+ * @typedef {Object} Product
+ * @property {string} category
+ * @property {string} id
+ * @property {string} name
+ * @property {number} price
+ * @property {boolean} stocked
+ */
+
+/**
+ * 商品追加フォームの作成、入力値を受け取る処理（UI担当）
+ * @param {Object} props
+ * @param {Product[]} props.products
+ * @param {(value: Product[])=> void} props.onProductsChange
+ * @returns {JSX.Element}
+ */
+// onProductsChangeはsetStateの関数を渡している→型の書き方がちょっと違う！
+type Props = {
+  products: Product[];
+  onProductsChange: (value: Product[]) => void;
+};
+export default function AddProductForm({ products, onProductsChange }: Props) {
+  // ローカルのstateを作成する
+  const [productCategory, onProductCategoryChange] = useState("Fruits");
+  // ! この子はe.target.valueでもらってくるから、文字列がよろしい
+  const [productPrice, onProductPriceChange] = useState("");
+  const [isProductStock, onIsProductStockChange] = useState(false);
+  const [productName, onProductNameChange] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  // フォームを送信するときに実行する処理
+  function handleSubmit(e) {
+    // 再描画を防ぐ
+    e.preventDefault();
+    if (productPrice === "") {
+      setErrorMessage("Please enter the price");
+      return;
+    }
+    const validationError = validationPrice(productPrice);
+    if (validationError) {
+      setErrorMessage(validationError);
+      return;
+    }
+    // 商品名の空白を削除する
+    const trimmedName = productName.trim();
+    const nameError = validationTrimmedName(trimmedName);
+    if (nameError) {
+      setErrorMessage(nameError);
+      return;
+    }
+    setErrorMessage(null);
+    const newProduct = {
+      id: crypto.randomUUID(),
+      category: productCategory,
+      price: Number(productPrice),
+      stocked: isProductStock,
+      name: trimmedName,
+    };
+
+    // FilterableProductTableにわたすための一時データ
+    const newProducts = [...products, newProduct];
+    onProductsChange(newProducts);
+  }
+  // onProductsChange(newProducts);
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        {/* セレクトはどうやって入力を受け取る？ */}
+        <div className="mb-3">
+          {/* ラベルにもclassName="form-label"があるとラベルとinputの隙間が整う */}
+          <label htmlFor="category-select" className="form-label">
+            Category
+          </label>
+          <select
+            name="category"
+            id="category-select"
+            value={productCategory}
+            onChange={(e) => {
+              onProductCategoryChange(e.target.value);
+            }}
+            required
+            className="form-select"
+          >
+            <option value="Fruits">Fruits</option>
+            <option value="Vegetables">Vegetables</option>
+            <option value="Snacks">Snacks</option>
+          </select>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="product-price" className="form-label">
+            Product Price
+          </label>
+          <div className="input-group">
+            <span className="input-group-text">$</span>
+            <input
+              name="productPrice"
+              type="number"
+              value={productPrice}
+              onChange={(e) => {
+                onProductPriceChange(e.target.value);
+              }}
+              max={100000}
+              min={1}
+              required
+              className="form-control"
+              id="product-price"
+            />
+          </div>
+        </div>
+        <div className="form-check mb-3">
+          <input
+            name="stockedCheckbox"
+            type="checkbox"
+            checked={isProductStock}
+            onChange={(e) => {
+              onIsProductStockChange(e.target.checked);
+            }}
+            className="form-check-input"
+            id="add-stock-checkbox"
+          />
+          {/* チェックボックスのときのラベルはform-check-label */}
+          <label htmlFor="add-stock-checkbox" className="form-check-label">
+            Do you have these items in stock?
+          </label>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="product-name" className="form-label">
+            Product name
+          </label>
+          <input
+            name="productName"
+            type="text"
+            placeholder="Add..."
+            value={productName}
+            onChange={(e) => {
+              onProductNameChange(e.target.value);
+            }}
+            maxLength={30}
+            minLength={1}
+            required
+            className="form-control"
+            id="product-name"
+          ></input>
+        </div>
+        {/* trueのときにHTML書くなら()で囲む */}
+        {errorMessage ? (
+          <p className="error-message text-danger" role="alert">
+            {errorMessage}
+          </p>
+        ) : null}
+        <button type="submit" className="btn btn-primary mb-3">
+          add product
+        </button>
+      </form>
+    </>
+  );
+}
