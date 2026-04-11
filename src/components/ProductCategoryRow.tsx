a5f48b7bb66ff7fc7dfcec62f9d10f970e768b01
IchikaCoding <IchikaCoding@users.noreply.github.com>
2026-04-11 21:20:44 +0900

    fix: Propsの型定義を修正し、分割代入を適用

--- src/components/ProductCategoryRow.tsx
+++ src/components/ProductCategoryRow.tsx
@@ -1,10 +1,20 @@
+// 分割代入
+
 // コンポーネントだから返り値省略
 // TODO: プロパティ 'category' は型 'String' に存在しません。とエラーがでた
-export default function ProductCategoryRow({ category }: string) {
+// コンポーネントがPropsを受け取るためには分割代入！
+// `category: string`だとProps全体がstring型の意味になってしまう
+// Props全体は{ category: "Fruits" }になる！→引数はオブジェクトになる！
+
+type Props = {
+  category: string;
+};
+export default function ProductCategoryRow({ category }: Props) {
   return (
     <tr className="text-muted small py-2 border-top border-2">
       {/* TODO: 「型stringを型numberに割り当てることは出来ません」と言われた。 */}
-      <th colSpan="4" className="fw-bold text-start">
+      {/* どうして数値型だと{}で囲むの？ */}
+      <th colSpan={4} className="fw-bold text-start">
         {category}
       </th>
     </tr>
