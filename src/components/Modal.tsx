ae7d5a0cd542b880bca34045fe99968f149573c1
IchikaCoding <IchikaCoding@users.noreply.github.com>
2026-04-12 22:05:16 +0900

    fix: Modalコンポーネントをコピペ。引数の型を追加。handleEscapeKeyDownの引数のイベント型も追加。

---
+++ src/components/Modal.tsx
@@ -0,0 +1,150 @@
+import { useEffect, useRef } from "react";
+
+/**
+ * 削除ボタンを押したときのモーダル表示処理
+ * @param {Object} props
+ * @param {boolean} props.isModalOpen - モーダルを開くかどうか
+ * @param {() => void} props.onConfirm - OK時の処理
+ * @param {() => void} props.onCancel - キャンセル時の処理
+ * @returns {JSX.Element | null} モーダルUI（非表示時はnull）
+ */
+
+type Props = {
+  isModalOpen: boolean;
+  onConfirm: () => void;
+  onCancel: () => void;
+};
+export default function Modal({ isModalOpen, onConfirm, onCancel }: Props) {
+  // closeButtonRefはモーダルの要素の中身だからこのコンポーネントで宣言したってこと
+  // ! closeButtonRef.currentのDOM要素には画面描画後に入る
+  const closeButtonRef = useRef(null);
+
+  // イベントリスナー登録したい！これは画面描画以外👉️useEffectの出番！
+  // TODO: これって最後に処理される？
+  useEffect(() => {
+    // モーダルが閉じていたら早期リターンしてイベント登録しない
+    if (!isModalOpen) return;
+    // エスケープキーのハンドラ
+    // addEventListenerでキーを検知している→ブラウザ標準のイベント！
+    // Event型にkeyはない→KeyboardEvent型になる！
+    function handleEscapeKeyDown(e: KeyboardEvent) {
+      if (e.key === "Escape") {
+        return onCancel();
+      }
+    }
+    // 角煮
+    console.log("closeButtonRef", closeButtonRef);
+    document.addEventListener("keydown", handleEscapeKeyDown);
+    // イベントの処理が終わったらreturnのうしろにイベントの解除の処理を書く！
+    return () => {
+      document.removeEventListener("keydown", handleEscapeKeyDown);
+    };
+    // useEffect内で使ったから書いた
+  }, [isModalOpen, onCancel]);
+
+  // TODO: Modalのフォーカスと背景スクロールの制御のuseEffectを書く
+  // ここで書いていいのかわからないけど書く
+  useEffect(() => {
+    const appContent = document.getElementById("app-content");
+    if (!appContent) return;
+    // Modalが開いている場合のコード
+    if (isModalOpen) {
+      // 背景スクロールをNGにする
+      // どうしたらこれ検索できる？
+      document.body.style.overflow = "hidden";
+      // Modalの閉じるボタンにフォーカス
+      closeButtonRef.current?.focus();
+      // inert属性とはその要素の中を「操作できない状態」にする属性
+      // setAttributeは第1引数に属性名、第2引数に値を書く
+      appContent.setAttribute("inert", "");
+      // 読み上げ機能に読み上げさせたくない場所として隠すなどため
+      // inert属性で十分だけど、念の為、支援技術に対してだけ隠す属性もつけて二重ガードしている
+      appContent.setAttribute("aria-hidden", "true");
+    } else {
+      // ここが動くのはモーダルが閉じるとき（削除確定時、Cancelしたとき）
+      document.body.style.overflow = "";
+      // removeAttributeは第1引数に属性名を書いてその属性を削除する
+      appContent.removeAttribute("inert");
+      appContent.removeAttribute("aria-hidden");
+    }
+    return () => {
+      document.body.style.overflow = "";
+      appContent.removeAttribute("inert");
+      appContent.removeAttribute("aria-hidden");
+    };
+  }, [isModalOpen]);
+
+  if (!isModalOpen) {
+    // React のコンポーネントの return なら null を返す
+    return null;
+  }
+  // returnされるもの
+  return (
+    <>
+      {/* .modal自体の標準はdisplay: none;（非表示寄り）。だからd-blockは表示するやつをつける*/}
+      <div
+        // fade: アニメーション用の「初期状態」（透明 or ずらし）
+        // show: 表示状態にするクラス（fade とセットで使う）
+        className="modal d-block fade show"
+        tabIndex={-1}
+        role="dialog"
+        aria-modal="true"
+        // これつけることで、ダイアログの外側をクリックしたら閉じる
+        onClick={onCancel}
+        // modal-titleのidと同じ名前をつけておくとアクセシビリティ的にGoodなの？
+        aria-labelledby="ModalLabel"
+      >
+        {/* モーダル画面をだしているときに暗くなる背景の部分。ここをクリックするとキャンセルされる */}
+        {/* e.stopPropagation()でダイアログ画面の内側のクリックを外側に伝えないようにしている */}
+        <div
+          className="modal-dialog modal-dialog-centered"
+          onClick={(e) => e.stopPropagation()}
+        >
+          {/* モーダル画面本体。e.stopPropagation()によって
+      ダイアログがクリックされても背景までクリックされたことが伝わらない！
+      だから、ダイアログを押してもダイアログが閉じることがなくなる！
+      */}
+          <div className="modal-content">
+            <div className="modal-header">
+              {/* ページ本文でh1使っている場合、見出しの階層を壊しにくくするためにh2~3くらいにするのがよいらしい */}
+              <h2 className="modal-title" id="ModalLabel">
+                Confirm delete
+              </h2>
+              <button
+                aria-label="Close dialog"
+                type="button"
+                className="btn-close"
+                onClick={onCancel}
+                ref={closeButtonRef}
+              ></button>
+            </div>
+            <div className="modal-body">
+              <p>Are you sure you want to delete?</p>
+            </div>
+            <div className="modal-footer">
+              {/* ここはボタン要素にテキストがあるからaria-label は基本つけない。
+              同時に付けると、見えている文字と読み上げ名がズレるリスクがある */}
+              <button
+                type="button"
+                onClick={onCancel}
+                className="btn btn-secondary"
+              >
+                Cancel
+              </button>
+              <button
+                type="button"
+                onClick={onConfirm}
+                className="btn btn-danger"
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* fadeはアニメーションの仕組みを担当。モーダルにshowをつけると、表示状態のスタイル（背景色とか濃さとか）などを調整してくれる */}
+      {/* これはモーダル開いたときに暗くする背景レイヤー。モーダルの要素とは別でdiv要素を作成している */}
+      <div className="modal-backdrop show"></div>
+    </>
+  );
+}
